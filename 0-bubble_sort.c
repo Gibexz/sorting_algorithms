a52815334c91cc90005be4f41f6b7cf6d002e7a9
Gibexz <gibexzetedu@gmail.com>
2023-04-21 17:55:40 +0100

    Big O Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+void swap(int* xp, int* yp);
+/**
+ *
+ *
+ *
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    print_array(array, size);
+    for (i = 0; i < size - 1; i++)
+        /*Last i elements are already in place*/
+        for (j = 0; j < size - i - 1; j++)
+            if (array[j] > array[j + 1])
+	    {
+                swap(&array[j], &array[j + 1]);
+		print_array(array, size);
+	    }
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+void swap(int *arr1, int *arr2)
+{
+    int temp = *arr1;
+    *arr1 = *arr2;
+    *arr2 = temp;
+}
