4ab97a294d0b054cb4c97a8a29a2061a39602688
Gibexz <gibexzetedu@gmail.com>
2023-04-25 01:52:11 +0100

    Task 0 updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,11 +23,11 @@ void bubble_sort(int *array, size_t size)
 		/*Last i elements are already in place*/
 		for (j = 0; j < size - i - 1; j++)
 		{
-        		if (array[j] > array[j + 1])
-	    		{
-        			swap(&array[j], &array[j + 1]);
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
-	    		}
+			}
 		}
     	}
 }
