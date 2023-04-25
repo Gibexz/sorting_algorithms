2c33fdafc83ca71a7015c5d22b6a6295f62b7e19
Gibexz <andrewetedu@gmail.com>
2023-04-25 01:12:41 +0000

    Task 2 updates

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,43 +11,41 @@ void swap(int *arr1, int *arr2);
  */
 void selection_sort(int *array, size_t size)
 {
-
-    	size_t i, j, mini_idx;
+	size_t i, j, mini_idx;
 
 	if (array == NULL)
 		return;
 	if (size < 2)
 		return;
 
-    	/* loop for sorting boundary movement */
-   	for (i = 0; i < size - 1; i++)
-    	{
-
-        	/* To find the minimum value in the array*/
+	/* loop for sorting boundary movement */
+	for (i = 0; i < size - 1; i++)
+	{
+		/* To find the minimum value in the array*/
 		mini_idx = i;
-        	for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
-           		if (array[j] < array[mini_idx])
+			if (array[j] < array[mini_idx])
 				mini_idx = j;
 		}
 		if (mini_idx != i)
-                {
-                        swap(&array[mini_idx], &array[i]);
-                        print_array(array, size);
-                }
-    }
+		{
+			swap(&array[mini_idx], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
 
 /**
  * swap - function to swap two values in an array
  * @arr1: higher value
  * @arr2: lower value
- * 
+ *
  * Return: Nothing
  */
 void swap(int *arr1, int *arr2)
 {
-    int temp = *arr1;
-    *arr1 = *arr2;
-    *arr2 = temp;
+	int temp = *arr1;
+	*arr1 = *arr2;
+	*arr2 = temp;
 }
