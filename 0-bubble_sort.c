fc89a2c43f8e0efbce19c5b8ba6c731029d30e56
Gibexz <gibexzetedu@gmail.com>
2023-04-25 01:05:07 +0100

    Task 0-bubbleSort updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,11 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
 
+    if (array == NULL)
+	    return;
+    if (size < 2)
+	    return;
+
     for (i = 0; i < size - 1; i++)
     {
         /*Last i elements are already in place*/
